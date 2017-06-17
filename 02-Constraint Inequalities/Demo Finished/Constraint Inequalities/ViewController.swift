e7caac4a6dcce365307b51eb4421163d6b2c8aae
dennisvera <dennisvera.z@gmail.com>
2017-06-17 16:36:08 -0400

    constraints inequality

---
+++ 02-Constraint Inequalities/Demo Finished/Constraint Inequalities/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Constraint Inequalities
+//
+//  Created by Jerry Beers on 1/23/17.
+//  Copyright © 2017 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
