eaeab8a39466cda893693e4dd1e6510a98c91f1b
dennisvera <dennisvera.z@gmail.com>
2017-06-27 11:04:26 -0400

    size class

---
+++ 07-Size Classes/SizeClassesChallenge/SizeClassesChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassesChallenge
+//
+//  Created by Dennis Vera on 6/27/17.
+//  Copyright © 2017 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
