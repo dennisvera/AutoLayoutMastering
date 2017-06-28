7336a43d49381b0a9aa8f0ed54b9c57244c8ad47
dennisvera <dennisvera.z@gmail.com>
2017-06-28 08:05:54 -0400

    adaptive layout environment

---
+++ 09-Adaptive Layout/Demo Finished/PirateHandbook/AppearanceManager.swift
@@ -0,0 +1,19 @@
+//
+//  AppearanceManager.swift
+//  PirateHandbook
+//
+//  Created by Jerry Beers on 1/24/17.
+//  Copyright © 2017 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class AppearanceManager {
+    class func setupAppearance() {
+        let navBarAppearance = UINavigationBar.appearance(for: UITraitCollection(horizontalSizeClass: .unspecified))
+        navBarAppearance.titleTextAttributes = [NSFontAttributeName: UIFont(name: "MarkerFelt-Wide", size: 22)!]
+        
+        let navBarCompactAppearance = UINavigationBar.appearance(for: UITraitCollection(horizontalSizeClass: .compact))
+        navBarCompactAppearance.titleTextAttributes = [NSFontAttributeName: UIFont(name: "MarkerFelt-Thin", size: 14)!]
+    }
+}
